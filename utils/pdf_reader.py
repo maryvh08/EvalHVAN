4f0b9f5a8a489cecef6358cbfb5bc7a2e8f249b9
MaryVH08 <marycarmen.vives@aneiap.co>
2025-11-29 02:35:54 -0500

    Add pdf_to_text function to extract text from PDFs

---
+++ utils/pdf_reader.py
@@ -0,0 +1,8 @@
+import PyPDF2
+
+def pdf_to_text(pdf_file):
+    reader = PyPDF2.PdfReader(pdf_file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+    return text.lower()
